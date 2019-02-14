442b630d75de65577f74621c58d40167194380b7
渊虹 <41134298+yuanhong18@users.noreply.github.com>
2019-02-14 11:08:32 +0800

    Create bubble.cpp

---
+++ sort/bubble.cpp
@@ -0,0 +1,22 @@
+/*
+比较相邻的元素。如果第一个比第二个大，就交换它们两个；
+对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对，这样在最后的元素应该会是最大的数；
+针对所有的元素重复以上的步骤，除了最后一个；
+重复步骤1~3，直到排序完成。
+*/
+void bubble_sort(vector<int> &vec) 
+{
+	int len = vec.size();
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = 0; j < len - i - 1; j++)
+		{
+			if (vec[j] > vec[j + 1])
+			{
+				int temp = vec[j];
+				vec[j] = vec[j + 1];
+				vec[j + 1] = temp;
+			}
+		}
+	}
+}
